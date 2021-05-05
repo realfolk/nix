8ef7fe537fbeea644faeb818e6654220ac9bc37c
Dhruv Dang <dhruv@realfolk.com>
2021-05-05 16:51:16 -0700

    add build-optimized command to static projects

--- lib/projects/static/default.nix
+++ lib/projects/static/default.nix
@@ -12,7 +12,7 @@ projectTypeId = "static";
 
 makeBuildDir = project: dirName: "${project.buildPath}/${projectTypeId}/${dirName}";
 
-commands = {
+commands = rec {
   build = project: projectsLib.makeCommand {
     inherit project;
     name = "${projectTypeId}-build";
@@ -22,6 +22,46 @@ commands = {
       rm -rf "${project.buildPath}"
       cp -r "${project.srcPath}" "${project.buildPath}"
       echo "Successfully copied assets to new build directory: ${project.buildPath}"
+    '';
+  };
+
+  build-optimized = project: projectsLib.makeCommand {
+    inherit project;
+    name = "${projectTypeId}-build-optimized";
+    script = ''
+      echo "--- Start optimized build ---"
+      ${(build project).bin}
+      cd ${project.buildPath}
+
+      echo "--- Minify JS files ---"
+      replace_suffix() {
+        file="$1"
+        old_suffix="$2"
+        new_suffix="$3"
+        echo "$(dirname $file)/$(basename $file $old_suffix)$new_suffix"
+      }
+      export -f replace_suffix
+      find -iname '*.js' -exec bash -c 'echo "Minifying $1" && ${pkgs.closurecompiler}/bin/closure-compiler --js $1 --js_output_file $(replace_suffix $1 ".js" ".min.js") && echo "Minified $(replace_suffix $1 ".js" ".min.js")"' bash {} \;
+
+      echo "--- Compress assets ---"
+      zopfli_compress() {
+        file="$1"
+        echo "Compressing (zopfli): $file"
+        ${pkgs.zopfli}/bin/zopfli "$1"
+        echo "Compressed (zopfli): $file"
+      }
+      brotli_compress() {
+        file="$1"
+        echo "Compressing (brotli): $file"
+        ${pkgs.brotli}/bin/brotli "$1"
+        echo "Compressed (brotli): $file"
+      }
+      export -f zopfli_compress
+      export -f brotli_compress
+      find . \( -iname '*.js' -o -iname '*.css' -o -iname '*.png' -o -iname '*.jpg' -o -iname '*.svg' -o -iname '*.html' -o -iname   '*.mdrn' -o -iname '*.json' \) -exec bash -c 'zopfli_compress $1; brotli_compress $1' bash {} \;
+
+      echo "--- Successfully completed optimized build ---"
+      echo "Build files written to ${project.buildPath}"
     '';
   };
 };
@@ -37,6 +77,7 @@ projectConfig = {
     projectsLib.commonCommands.ls-src
     projectsLib.commonCommands.ls-build
     commands.build
+    commands.build-optimized
   ];
 };
 
