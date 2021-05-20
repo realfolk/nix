28785119d2318412b60a937f13c7927c6f89d012
Dhruv Dang <dhruv@realfolk.com>
2021-05-19 22:09:42 -0700

    update nixpkgs rev to head

--- lib/config/nixpkgs.nix
+++ lib/config/nixpkgs.nix
@@ -7,6 +7,6 @@ in
 systemPkgs.fetchFromGitHub {
   owner = "nixos";
   repo = "nixpkgs-channels";
-  rev = "c59ea8b8a0e7f927e7291c14ea6cd1bd3a16ff38";
-  sha256 = "1ak7jqx94fjhc68xh1lh35kh3w3ndbadprrb762qgvcfb8351x8v";
+  rev = "b34ab9683cee9f51dfe2ad97bc5632d2e3683bd4";
+  sha256 = "0gryhigjcxv9z1b9jv4hdsl8pk3wcdc8g3ysp258yqfg5qkfnxbf";
 }
