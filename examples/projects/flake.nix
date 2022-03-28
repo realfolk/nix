71838b85d72998ee620ca64805c882c03b24ec39
Dhruv Dang <dhruv@realfolk.com>
2022-03-28 12:48:00 -0700

    nix,examples/projects: update flake.nix to not follow nixpkgs for inputs

--- examples/projects/flake.nix
+++ examples/projects/flake.nix
@@ -2,214 +2,177 @@
   description = "An example of a development shell using Real Folk's project flakes.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flakeUtils.url = "github:numtide/flake-utils";
-
-    neovim = {
-      url = "github:realfolk/nix?dir=lib/packages/neovim";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flakeUtils.follows = "flakeUtils";
-    };
-
-    tmux = {
-      url = "github:realfolk/nix?dir=lib/packages/tmux";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flakeUtils.follows = "flakeUtils";
-    };
-
-    ranger = {
-      url = "github:realfolk/nix?dir=lib/packages/ranger";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flakeUtils.follows = "flakeUtils";
-    };
-
-    elmPackages.url = "github:realfolk/nix?dir=lib/projects/elm/packages";
+    neovim.url = "github:realfolk/nix?dir=lib/packages/neovim";
+    tmux.url = "github:realfolk/nix?dir=lib/packages/tmux";
+    ranger.url = "github:realfolk/nix?dir=lib/packages/ranger";
+    elmPackages.url = "github:realfolk/nix?dir=lib/projects/elm/packages/elm-0.19";
     haskellPackages.url = "github:realfolk/nix?dir=lib/projects/haskell/packages/ghc-9.2.1";
     nodeInterpreter.url = "github:realfolk/nix?dir=lib/projects/node/interpreter/node-17";
-
-    commonProject = {
-      url = "github:realfolk/nix?dir=lib/projects/common";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    elmProject = {
-      url = "github:realfolk/nix?dir=lib/projects/elm";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    haskellProject = {
-      url = "github:realfolk/nix?dir=lib/projects/haskell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    staticProject = {
-      url = "github:realfolk/nix?dir=lib/projects/static";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nodeProject = {
-      url = "github:realfolk/nix?dir=lib/projects/node";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    commonProject.url = "github:realfolk/nix?dir=lib/projects/common";
+    elmProject.url = "github:realfolk/nix?dir=lib/projects/elm";
+    haskellProject.url = "github:realfolk/nix?dir=lib/projects/haskell";
+    staticProject.url = "github:realfolk/nix?dir=lib/projects/static";
+    nodeProject.url = "github:realfolk/nix?dir=lib/projects/node";
     projectLib.url = "github:realfolk/nix?dir=lib/projects/lib";
   };
 
-  outputs = {
-      self,
-      nixpkgs,
-      flakeUtils,
-      neovim,
-      tmux,
-      ranger,
-      elmPackages,
-      haskellPackages,
-      nodeInterpreter,
-      commonProject,
-      elmProject,
-      haskellProject,
-      nodeProject,
-      staticProject,
-      projectLib,
-      ...
+  outputs =
+    { self
+    , nixpkgs
+    , flakeUtils
+    , neovim
+    , tmux
+    , ranger
+    , elmPackages
+    , haskellPackages
+    , nodeInterpreter
+    , commonProject
+    , elmProject
+    , haskellProject
+    , nodeProject
+    , staticProject
+    , projectLib
+    , ...
     }:
     flakeUtils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        config = {
-          srcDir = "$PROJECT/src";
-          buildDir = "$PROJECT/build";
-          buildArtifactsDir = "$PROJECT/artifacts";
+      config = {
+        srcDir = "$PROJECT/src";
+        buildDir = "$PROJECT/build";
+        buildArtifactsDir = "$PROJECT/artifacts";
+      };
+
+      defineProject = args: projectLib.lib.defineProject (args // config);
+
+      defineElmProject = args: elmProject.lib.defineProject (args // config);
+
+      defineHaskellProject = args: haskellProject.lib.defineProject (args // config);
+
+      defineStaticProject = args: staticProject.lib.defineProject (args // config);
+
+      defineNodeProject = args: nodeProject.lib.defineProject (args // config);
+
+      testElmProjectDefinition = {
+        groupName = "group";
+        projectName = "testelm";
+        entryPoints = {
+          main = "Main.elm";
         };
+      };
 
-        defineProject = args: projectLib.lib.defineProject (args // config);
+      testElmProjectElm = elmProject.lib.make {
+        inherit system;
+        elmPackages = elmPackages.packages.${system};
+        project = defineElmProject testElmProjectDefinition;
+      };
 
-        defineElmProject = args: elmProject.lib.defineProject (args // config);
+      testElmProjectCommon = commonProject.lib.make {
+        inherit system;
+        project = defineProject testElmProjectDefinition;
+      };
 
-        defineHaskellProject = args: haskellProject.lib.defineProject (args // config);
-
-        defineStaticProject = args: staticProject.lib.defineProject (args // config);
-
-        defineNodeProject = args: nodeProject.lib.defineProject (args // config);
-
-        testElmProjectDefinition = {
-          groupName = "group";
-          projectName = "testelm";
-          entryPoints = {
-            main = "Main.elm";
-          };
+      testHaskellProjectDefinition = {
+        groupName = "group";
+        projectName = "testhaskell";
+        executables = {
+          main = "Main.hs";
         };
+        haskellDependencies = p: with p; [
+          aeson
+          cryptonite
+        ];
+      };
 
-        testElmProjectElm = elmProject.lib.make {
-          inherit system;
-          elmPackages = elmPackages.packages.${system};
-          project = defineElmProject testElmProjectDefinition;
-        };
+      testHaskellProjectHaskell = haskellProject.lib.make {
+        inherit system;
+        haskellPackages = haskellPackages.packages.${system};
+        project = defineHaskellProject testHaskellProjectDefinition;
+      };
 
-        testElmProjectCommon = commonProject.lib.make {
-          inherit system;
-          project = defineProject testElmProjectDefinition;
-        };
+      testHaskellProjectCommon = commonProject.lib.make {
+        inherit system;
+        project = defineProject testHaskellProjectDefinition;
+      };
 
-        testHaskellProjectDefinition = {
-          groupName = "group";
-          projectName = "testhaskell";
-          executables = {
-            main = "Main.hs";
-          };
-          haskellDependencies = p: with p; [
-            aeson
-            cryptonite
-          ];
-        };
+      testStaticProjectDefinition = {
+        groupName = "group";
+        projectName = "teststatic";
+      };
 
-        testHaskellProjectHaskell = haskellProject.lib.make {
-          inherit system;
-          haskellPackages = haskellPackages.packages.${system};
-          project = defineHaskellProject testHaskellProjectDefinition;
-        };
+      testStaticProjectStatic = staticProject.lib.make {
+        inherit system;
+        project = defineStaticProject testStaticProjectDefinition;
+      };
 
-        testHaskellProjectCommon = commonProject.lib.make {
-          inherit system;
-          project = defineProject testHaskellProjectDefinition;
-        };
+      testStaticProjectCommon = commonProject.lib.make {
+        inherit system;
+        project = defineProject testStaticProjectDefinition;
+      };
 
-        testStaticProjectDefinition = {
-          groupName = "group";
-          projectName = "teststatic";
+      testNodeProjectDefinition = {
+        groupName = "group";
+        projectName = "testnode";
+        executables = {
+          index = "index.js";
         };
+      };
 
-        testStaticProjectStatic = staticProject.lib.make {
-          inherit system;
-          project = defineStaticProject testStaticProjectDefinition;
-        };
+      testNodeProjectNode = nodeProject.lib.make {
+        inherit system;
+        nodeInterpreter = nodeInterpreter.packages.${system}.default;
+        project = defineNodeProject testNodeProjectDefinition;
+      };
 
-        testStaticProjectCommon = commonProject.lib.make {
-          inherit system;
-          project = defineProject testStaticProjectDefinition;
-        };
+      testNodeProjectCommon = commonProject.lib.make {
+        inherit system;
+        project = defineProject testNodeProjectDefinition;
+      };
+    in
+    {
+      packages = {
+        neovim = neovim.packages.${system}.default;
+        tmux = tmux.packages.${system}.default;
+        ranger = ranger.packages.${system}.default;
+      };
 
-        testNodeProjectDefinition = {
-          groupName = "group";
-          projectName = "testnode";
-          executables = {
-            index = "index.js";
-          };
-        };
-
-        testNodeProjectNode = nodeProject.lib.make {
-          inherit system;
-          nodeInterpreter = nodeInterpreter.packages.${system}.default;
-          project = defineNodeProject testNodeProjectDefinition;
-        };
-
-        testNodeProjectCommon = commonProject.lib.make {
-          inherit system;
-          project = defineProject testNodeProjectDefinition;
-        };
-      in
-      {
-        packages = {
-          neovim = neovim.packages.${system}.default;
-          tmux = tmux.packages.${system}.default;
-          ranger = ranger.packages.${system}.default;
-        };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            self.packages.${system}.neovim
-            self.packages.${system}.tmux
-            self.packages.${system}.ranger
-            #Elm
-            elmPackages.packages.${system}.elm
-            elmPackages.packages.${system}.elm-language-server
-            elmPackages.packages.${system}.elm-format
-            elmPackages.packages.${system}.elm-test
-            testElmProjectElm.combinedCommandsPackage
-            testElmProjectCommon.combinedCommandsPackage
-            #Haskell
-            haskellPackages.packages.${system}.ghc
-            haskellPackages.packages.${system}.haskell-language-server
-            testHaskellProjectHaskell.combinedCommandsPackage
-            testHaskellProjectCommon.combinedCommandsPackage
-            #Static Assets
-            testStaticProjectStatic.combinedCommandsPackage
-            testStaticProjectCommon.combinedCommandsPackage
-            #Node.js
-            nodeInterpreter.packages.${system}.default
-            testNodeProjectNode.combinedCommandsPackage
-            testNodeProjectCommon.combinedCommandsPackage
-          ];
-          shellHook = ''
-            test -f ~/.bashrc && source ~/.bashrc
-            export PROJECT="$PWD"
-            ${testElmProjectCommon.commands.mkdirSrc.bin}
-            ${testHaskellProjectCommon.commands.mkdirSrc.bin}
-            ${testHaskellProjectHaskell.commands.hieYaml.bin}
-            ${testStaticProjectCommon.commands.mkdirSrc.bin}
-            ${testNodeProjectCommon.commands.mkdirSrc.bin}
-          '';
-        };
-      });
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          self.packages.${system}.neovim
+          self.packages.${system}.tmux
+          self.packages.${system}.ranger
+          #Elm
+          elmPackages.packages.${system}.elm
+          elmPackages.packages.${system}.elm-language-server
+          elmPackages.packages.${system}.elm-format
+          elmPackages.packages.${system}.elm-test
+          testElmProjectElm.combinedCommandsPackage
+          testElmProjectCommon.combinedCommandsPackage
+          #Haskell
+          haskellPackages.packages.${system}.ghc
+          haskellPackages.packages.${system}.haskell-language-server
+          testHaskellProjectHaskell.combinedCommandsPackage
+          testHaskellProjectCommon.combinedCommandsPackage
+          #Static Assets
+          testStaticProjectStatic.combinedCommandsPackage
+          testStaticProjectCommon.combinedCommandsPackage
+          #Node.js
+          nodeInterpreter.packages.${system}.default
+          testNodeProjectNode.combinedCommandsPackage
+          testNodeProjectCommon.combinedCommandsPackage
+        ];
+        shellHook = ''
+          test -f ~/.bashrc && source ~/.bashrc
+          export PROJECT="$PWD"
+          ${testElmProjectCommon.commands.mkdirSrc.bin}
+          ${testHaskellProjectCommon.commands.mkdirSrc.bin}
+          ${testHaskellProjectHaskell.commands.hieYaml.bin}
+          ${testStaticProjectCommon.commands.mkdirSrc.bin}
+          ${testNodeProjectCommon.commands.mkdirSrc.bin}
+        '';
+      };
+    });
 }
--- flake.nix
+++ flake.nix
@@ -4,71 +4,53 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flakeUtils.url = "github:numtide/flake-utils";
-
-    neovim = {
-      url = "github:realfolk/nix?dir=lib/packages/neovim";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flakeUtils.follows = "flakeUtils";
-      };
-    };
-
-    ranger = {
-      url = "github:realfolk/nix?dir=lib/packages/ranger";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flakeUtils.follows = "flakeUtils";
-      };
-    };
-
-    rnixLsp = {
-      url = "github:nix-community/rnix-lsp";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    neovim.url = "github:realfolk/nix?dir=lib/packages/neovim";
+    ranger.url = "github:realfolk/nix?dir=lib/packages/ranger";
+    rnixLsp.url = "github:nix-community/rnix-lsp";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flakeUtils,
-    neovim,
-    ranger,
-    rnixLsp,
-    ...
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flakeUtils
+    , neovim
+    , ranger
+    , rnixLsp
+    , ...
+    }:
     flakeUtils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          rnixLsp = rnixLsp.defaultPackage.${system};
-          neovim = neovim.packages.${system}.default;
-          ranger = ranger.packages.${system}.default;
-        };
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages = {
+        rnixLsp = rnixLsp.defaultPackage.${system};
+        neovim = neovim.packages.${system}.default;
+        ranger = ranger.packages.${system}.default;
+      };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.silver-searcher # ag
-            pkgs.fzf
-            self.packages.${system}.rnixLsp
-            self.packages.${system}.neovim
-            self.packages.${system}.ranger
-          ];
-          shellHook = ''
-            # Load ~/.bashrc if it exists
-            test -f ~/.bashrc && source ~/.bashrc
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.silver-searcher # ag
+          pkgs.fzf
+          self.packages.${system}.rnixLsp
+          self.packages.${system}.neovim
+          self.packages.${system}.ranger
+        ];
+        shellHook = ''
+          # Load ~/.bashrc if it exists
+          test -f ~/.bashrc && source ~/.bashrc
 
-            # Source .env file if present
-            test -f "$PROJECT/.env" && source .env
+          # Source .env file if present
+          test -f "$PROJECT/.env" && source .env
 
-            # Ignore files specified in .gitignore when using fzf
-            # -t only searches text files and includes empty files
-            export FZF_DEFAULT_COMMAND="ag -tl"
+          # Ignore files specified in .gitignore when using fzf
+          # -t only searches text files and includes empty files
+          export FZF_DEFAULT_COMMAND="ag -tl"
 
-            # Initialize $PROJECT environment variable
-            export PROJECT="$PWD"
-          '';
-        };
-      });
+          # Initialize $PROJECT environment variable
+          export PROJECT="$PWD"
+        '';
+      };
+    });
 }
