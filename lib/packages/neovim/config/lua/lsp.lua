544c8f4111519f6e35188085b01e31115850b068
Dhruv Dang <dhruv@realfolk.com>
2022-05-05 22:06:06 -0700

    packages/neovim: fix <CR> and <Tab> cmp/autocompletion bindings

--- lib/packages/neovim/config/lua/lsp.lua
+++ lib/packages/neovim/config/lua/lsp.lua
@@ -115,25 +115,25 @@ local cmp = require 'cmp'
 cmp.setup {
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+      behavior = cmp.ConfirmBehavior.Replace
     },
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      -- Only jump locally, do not expand. cmp will show snippets that can be expanded.
+      elseif luasnip.in_snippet() and luasnip.jumpable(1) then
+        luasnip.jump(1)
       else
         fallback()
       end
@@ -141,7 +141,7 @@ cmp.setup {
     ['<S-Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      elseif luasnip.in_snippet() and luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
