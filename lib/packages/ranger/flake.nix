bef02e8135fc6297852b769551e051d9be09542d
Dhruv Dang <dhruv@realfolk.com>
2022-08-03 23:32:26 -0700

    packages/ranger: update flake.lock, dont run python tests

--- lib/packages/ranger/flake.nix
+++ lib/packages/ranger/flake.nix
@@ -22,39 +22,39 @@
         rifleConf = pkgs.writeText "rifle.conf" (builtins.readFile ./config/rifle.conf);
 
         ranger = python3Packages.buildPythonApplication rec {
-					pname = "ranger";
-					version = "master";
+          pname = "ranger";
+          version = "master";
           src = ranger-src;
-					LC_ALL = "en_US.UTF-8";
-					checkInputs = with python3Packages; [ pytestCheckHook ];
+          LC_ALL = "en_US.UTF-8";
+          doCheck = false;
 
-					propagatedBuildInputs = [ file python3Packages.astroid python3Packages.pylint ]
-						++ lib.optionals (imagePreviewSupport) [ python3Packages.pillow ];
+          propagatedBuildInputs = [ file python3Packages.astroid python3Packages.pylint ]
+            ++ lib.optionals (imagePreviewSupport) [ python3Packages.pillow ];
 
-					preConfigure = ''
-            #UPSTREAM
-						${lib.optionalString (highlight != null) ''
-							sed -i -e 's|^\s*highlight\b|${highlight}/bin/highlight|' \
-								ranger/data/scope.sh
-						''}
-						substituteInPlace ranger/__init__.py \
-							--replace "DEFAULT_PAGER = 'less'" "DEFAULT_PAGER = '${lib.getBin less}/bin/less'"
-						# give file previews out of the box
-						substituteInPlace ranger/config/rc.conf \
-							--replace /usr/share $out/share \
-							--replace "#set preview_script ~/.config/ranger/scope.sh" "set preview_script $out/share/doc/ranger/config/scope.sh"
-					'' + lib.optionalString imagePreviewSupport ''
-						substituteInPlace ranger/ext/img_display.py \
-							--replace /usr/lib/w3m ${w3m}/libexec/w3m
-						# give image previews out of the box when building with w3m
-						substituteInPlace ranger/config/rc.conf \
-							--replace "set preview_images false" "set preview_images true"
+          preConfigure = ''
+                        #UPSTREAM
+            						${lib.optionalString (highlight != null) ''
+            							sed -i -e 's|^\s*highlight\b|${highlight}/bin/highlight|' \
+            								ranger/data/scope.sh
+            						''}
+            						substituteInPlace ranger/__init__.py \
+            							--replace "DEFAULT_PAGER = 'less'" "DEFAULT_PAGER = '${lib.getBin less}/bin/less'"
+            						# give file previews out of the box
+            						substituteInPlace ranger/config/rc.conf \
+            							--replace /usr/share $out/share \
+            							--replace "#set preview_script ~/.config/ranger/scope.sh" "set preview_script $out/share/doc/ranger/config/scope.sh"
+            					'' + lib.optionalString imagePreviewSupport ''
+            						substituteInPlace ranger/ext/img_display.py \
+            							--replace /usr/lib/w3m ${w3m}/libexec/w3m
+            						# give image previews out of the box when building with w3m
+            						substituteInPlace ranger/config/rc.conf \
+            							--replace "set preview_images false" "set preview_images true"
 
-            # CUSTOM
-            # add custom rifle.conf
-            cat "${rifleConf}" > ranger/config/rifle.conf;
-					'';
-				};
+                        # CUSTOM
+                        # add custom rifle.conf
+                        cat "${rifleConf}" > ranger/config/rifle.conf;
+            					'';
+        };
 
       in
       {
